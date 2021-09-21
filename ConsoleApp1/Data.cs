b14dc66914b87b97fc2b49028025334ce0d8425f
Иброхим Гиязов <ibrox9656@gmail.com>
2021-09-21 20:27:56 +0300

    first

---
+++ ConsoleApp1/Data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    static class Data
+    {
+
+        public static Subject SubjectData { get; } = new Subject("Математика");
+
+        public static List<Subject> GetSubject()
+        {
+            List<Subject> subjects = new List<Subject>()
+            {
+                new Subject("Математика"),
+                new Subject("Русский"),
+                new Subject("ывпы"),
+                new Subject("ыа"),
+                new Subject("ыа"),
+                new Subject("йфка"),
+            };
+
+            return subjects;
+        }
+    }
+}
---
+++ ConsoleApp1/Subject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Subject
+    {
+        public string Name { get; set; }
+
+        public Subject(string name)
+        {
+            Name = name;
+        }
+    }
+}
